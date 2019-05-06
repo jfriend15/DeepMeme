1ae3f12a154066c0873804289ea747781873cd53
volson <volson@oberlin.edu>
2019-05-06 19:20:48 -0400

    fixed top/bottom separation

--- TextGen.py
+++ TextGen.py
@@ -4,47 +4,60 @@
 import sys
 import markov
 
+# builds a dictionary of strings (length k) : Markov link pairs
+def buildDict(inp, dict, k):
+    q = [] # an array to hold current substring length k + 1
+    c = inp.read(1)
+    while c: # while current character read in is not EOF, build dict
+        q.append(c)
+        if len(q) >= k+1:
+            a = q.pop(0)
+            for i in range(0, k-1):
+                a = a+q[i]
+            if a not in dict.keys(): # if current substring not in dictionary, create entry
+                mark = markov.Markov(a)
+                mark.add(q[k-1])
+                dict[a] = mark
+            else: # otherwise update entry with suffix
+                mark = dict[a]
+                mark.add(q[k-1])
+        c = inp.read(1)
+    return dict
+
 def main():
     if len(sys.argv) != 2: # produces an error if k, file not provided
         print("You need 1 argument!")
         sys.exit()
     k = int(sys.argv[1]) # number of characters per link
     inp = open("./Data/grammars.txt", "r") # read in file
-    dict = {} # a dictionary of strings (length k) : Markov link pairs
+    dict = {}
     try:
-        q = [] # an array to hold current substring length k + 1
-        c = inp.read(1)
-        while c: # while current character read in is not EOF, build dict
-            q.append(c)
-            if len(q) >= k+1:
-                a = q.pop(0)
-                for i in range(0, k-1):
-                    a = a+q[i]
-                if a not in dict.keys(): # if current substring not in dictionary, create entry
-                    mark = markov.Markov(a)
-                    mark.add(q[k-1])
-                    dict[a] = mark
-                else: # otherwise update entry with suffix
-                    mark = dict[a]
-                    mark.add(q[k-1])
-            c = inp.read(1)
+        dict = buildDict(inp, dict, k)
     except:
         print("Error reading from file: ./Data/grammars.txt")
     charList = "" # string to keep track of what should be printed/looked at
     inp.seek(1)
+    out = open("./Data/genGrammars.txt", "a+")
     for i in range(0, k): # sets up charList at beginning of file
         next = inp.read(1)
         if next != -1:
             charList = charList+str(next)
-            print(next, end="") # prints first k characters
+            out.write(next) # prints first k characters
     inp.close()
     ran = ""
-    out = open("./Data/genGrammars.txt", "a+")
+    while ran.find("|") == -1:
+        ma = dict[charList]
+        ran = ma.random() # chooses random suffix based on frequency in Markov link
+        charList = charList+str(ran)
+        charList = charList[1:len(charList)]
+        out.write(ran) # prints randomly chosen new character
     while ran.find(".") == -1:
         ma = dict[charList]
         ran = ma.random() # chooses random suffix based on frequency in Markov link
         charList = charList+str(ran)
         charList = charList[1:len(charList)]
+        if ran.find("|") != -1:
+            continue
         out.write(ran) # prints randomly chosen new character
     out.write("\n")
     out.close()
--- grammarParser.py
+++ grammarParser.py
@@ -7,19 +7,27 @@ import nltk
 
 def main():
     top = open("./Data/toptext.txt", "r")
-    texts = []
+    topTexts = []
+    bottomTexts = []
     for i in top: # reads in the top texts
         i = i[:-1]+" "
-        texts.append(i)
+        i = i.lower()
+        i = nltk.word_tokenize(i) # separates words in text (tokenizes)
+        i = nltk.pos_tag(i) # tags parts of speech in text
+        topTexts.append(i)
     k = 0
     top.close()
     bottom = open("./Data/bottomtext.txt", "r")
     w = open("./Data/grammars.txt", "w+")
     for j in bottom: # reads in the bottom texts, appends to top texts
-        texts[k] = texts[k]+j
-        texts[k] = nltk.word_tokenize(texts[k]) # separates words in text (tokenizes)
-        texts[k] = nltk.pos_tag(texts[k]) # tags parts of speech in text
-        for i in texts[k]: # writes tags for each meme grammar in grammars.txt
+        j = j.lower()
+        j = nltk.word_tokenize(j)
+        j = nltk.pos_tag(j)
+        bottomTexts.append(j)
+        for i in topTexts[k]: # writes tags for each meme grammar in grammars.txt
+            w.write(" "+i[1])
+        w.write(" |")
+        for i in bottomTexts[k]:
             w.write(" "+i[1])
         w.write(".\n")
         k += 1
