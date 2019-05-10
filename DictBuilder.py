c2f795c5535c630bb0561fde34bb60efc633c02a
Julia <jfriend@oberlin.edu>
2019-05-10 15:24:55 -0400

    fixed

--- DictBuilder.py
+++ DictBuilder.py
@@ -2,37 +2,62 @@
 
 import nltk
 import pickle
+import numpy as np 
+import string
 
-data_dict = {}
+def build_grammar_dict():
+	data_dict = {}
 
-text = []
-top = open("Data/toptext.txt", "r")
-bottom = open("Data/bottomtext.txt", "r")
-text.extend(top.readlines())
-text.extend(bottom.readlines())
-text = [t.strip() for t in text]
-# TODO strip better. take out commas and weird characters.
+	text = []
+	top = open("Data/toptext.txt", "r")
+	bottom = open("Data/bottomtext.txt", "r")
+	text.extend(top.readlines())
+	text.extend(bottom.readlines())
 
-words = [w for t in text for w in t.split(' ')]
-word_set = set(words)
+	punctuation = string.punctuation.replace("'", "")
 
-for w in word_set:
-	token = nltk.word_tokenize(w)
-	tag = nltk.pos_tag(token)[0][1]
+	text = [t.strip().lower() for t in text]
+	text = [t.translate(str.maketrans('', '', punctuation)) for t in text]
 
-	if tag in data_dict:
-		data_dict[tag].append(token)
-	else:
-		data_dict[tag] = [token]
-	
+	words = [w for t in text for w in t.split(' ')]
+	word_set = set(words)
+
+	for w in word_set:
+		token = nltk.word_tokenize(w)
+		tag = nltk.pos_tag(token)[0][1]
+
+		if tag in data_dict:
+			data_dict[tag].append(token)
+		else:
+			data_dict[tag] = [token]
+
+	return data_dict
+
+# https://stackoverflow.com/questions/37793118/load-pretrained-glove-vectors-in-python
+def loadGloveModel(gloveFile):
+    print("Loading Glove Model")
+    f = open(gloveFile,'r',encoding='utf-8')
+    model = {}
+    for line in f:
+        splitLine = line.split()
+        word = splitLine[0]
+        embedding = np.array([float(val) for val in splitLine[1:]])
+        model[word] = embedding
+    print("Done.",len(model)," words loaded!")
+    return model
 		
-print(data_dict.keys())
-print(data_dict['NNP'])
+def save_dict(dictionary, filename):
+	file = open('Data/' + filename + '.pkl', 'wb')
+	pickle.dump(dictionary, file)
+	file.close()
 
-# for w in word_set:
-	# find first/best 100 similar words in glove
-	# tokenize and tag
-	# add to bigger dictionary
 
-# save both dictionaries with pickle
+def main():
+	g = build_grammar_dict()
+	save_dict(g, 'grammarDict')
+	m = loadGloveModel('Data/glove.twitter.27B.25d.txt')
+	save_dict(m, 'gloveDict')
 
+
+if __name__ == '__main__':
+	main()
